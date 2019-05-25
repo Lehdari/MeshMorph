737dcb719a014bf10f79abdd1dc75d6839ab3ced
Lehdari <miika.lehtimaki@gmail.com>
2019-05-26 01:29:48 +0300

    Load and show bunny

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,17 @@
 // Created by lehdari on 26.5.2019.
 //
 
-int main(void)
+#include "OpenGLUtils/App.hpp"
+
+
+int main(int argv, char** args)
 {
+    App app;
+
+    Mesh bunny;
+    bunny.loadFromObj(std::string(GRAPHICSUTILS_RES_PATH) + "models/bunny.obj");
+    app.addMesh(std::move(bunny));
+    app.loop();
+
     return 0;
 }
